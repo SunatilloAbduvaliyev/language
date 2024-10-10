39c1e83616403bc3e80ba1cfff0fc099c0b0b4ff
mobile <codepython632@gmail.com>
2024-10-10 17:52:40 +0500

    quiz ui va funksiyalik qilish kerak

--- lib/screen/quiz_game/quiz_game_screen.dart
+++ lib/screen/quiz_game/quiz_game_screen.dart
@@ -1,46 +1,205 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:english/cubit/quiz_cubit/quiz_cubit.dart';
-import 'package:english/cubit/quiz_cubit/quiz_state.dart';
-import 'package:english/screen/widgets/global_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
-import '../../utils/extension/extension.dart';
 
 class QuizGameScreen extends StatefulWidget {
   const QuizGameScreen({super.key});
 
   @override
-  State<QuizGameScreen> createState() => _QuizGameScreenState();
+  _QuizGameScreenState createState() => _QuizGameScreenState();
 }
 
 class _QuizGameScreenState extends State<QuizGameScreen> {
+  int? selectedOptionIndex; // Bu tanlangan variantni saqlash uchun
+
+  void selectOption(int index) {
+    setState(() {
+      selectedOptionIndex = index; // Tanlangan variantni yangilash
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GlobalAppBar(
-        title: "quiz_game".tr(),
-        navigatePop: true,
-      ),
-      body: BlocBuilder<QuizCubit, QuizState>(
-        builder: (BuildContext context, QuizState state) {
-          return Column(
+      backgroundColor: Colors.blueGrey[900],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Center(
-                child: LinearPercentIndicator(
-                  width: width - 50,
-                  animation: true,
-                  lineHeight: 20.0,
-                  animationDuration: 2000,
-                  percent: state.quizIndex/state.gameQuiz.length,
-                  center: const Text("90.0%"),
-                  linearStrokeCap: LinearStrokeCap.roundAll,
-                  progressColor: Colors.greenAccent,
+              // Progress bar
+              LinearProgressIndicator(
+                value: 0.7,  // Example: 70% to'ldirilgan
+                backgroundColor: Colors.grey[800],
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+              ),
+              SizedBox(height: 30),
+
+              // Savol containeri
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blueAccent.withOpacity(0.7), Colors.purpleAccent.withOpacity(0.7)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueAccent.withOpacity(0.3),
+                      blurRadius: 30,
+                      spreadRadius: 5,
+                      offset: Offset(15, 10),
+                    ),
+                  ],
+                  border: Border.all(color: Colors.white.withOpacity(0.7), width: 2),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Savol:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Bu yerda savol bo‘ladi. Masalan: Flutter nima?',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+
+              // Javob variantlari
+              Column(
+                children: [
+                  OptionButton(
+                    optionText: "A variant: To'g'ri",
+                    gradientColors: [Colors.greenAccent, Colors.teal],
+                    isSelected: selectedOptionIndex == 0, // Tanlangan holatni tekshirish
+                    onTap: () => selectOption(0),
+                  ),
+                  OptionButton(
+                    optionText: "B variant: Noto'g'ri",
+                    gradientColors: [Colors.redAccent, Colors.pinkAccent],
+                    isSelected: selectedOptionIndex == 1,
+                    onTap: () => selectOption(1),
+                  ),
+                  OptionButton(
+                    optionText: "C variant: Noto'g'ri",
+                    gradientColors: [Colors.orangeAccent, Colors.deepOrange],
+                    isSelected: selectedOptionIndex == 2,
+                    onTap: () => selectOption(2),
+                  ),
+                  OptionButton(
+                    optionText: "D variant: Noto'g'ri",
+                    gradientColors: [Colors.purpleAccent, Colors.deepPurple],
+                    isSelected: selectedOptionIndex == 3,
+                    onTap: () => selectOption(3),
+                  ),
+                ],
+              ),
+              Spacer(),
+
+              // Keyingi tugmasi
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,  // Tugma rangi
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {
+                  // Keyingi savolga o'tish logikasi
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Keyingi',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Icon(Icons.arrow_forward),
+                  ],
                 ),
               ),
             ],
-          );
-        },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Option Button Widget
+class OptionButton extends StatelessWidget {
+  final String optionText;
+  final List<Color> gradientColors;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  OptionButton({
+    required this.optionText,
+    required this.gradientColors,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: InkWell(
+        onTap: onTap,
+        child: Ink(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: isSelected ? [Colors.lightGreen, Colors.green] : gradientColors,  // Tanlanganida rang o'zgarishi
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+              color: isSelected ? Colors.greenAccent : Colors.blueAccent,
+              width: 2,
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  optionText,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                if (isSelected)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                    ),
+                  ), // Tanlangan variantda check iconi qo'shish
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/screen/quiz_game/widget/quiz_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class QuizItem extends StatefulWidget {
+  const QuizItem({super.key});
+
+  @override
+  State<QuizItem> createState() => _QuizItemState();
+}
+
+class _QuizItemState extends State<QuizItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
